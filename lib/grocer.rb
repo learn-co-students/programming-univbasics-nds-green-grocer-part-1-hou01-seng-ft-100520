d3dd222bc71e448d95b9bcd0c7a9b4fe887d8419
Elijah Brooks <elijahcbrooks@gmail.com>
2020-09-22 20:21:01 +0000

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,17 +1,39 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
+  collection.each{|item|
+    if item[:item] == name then
+      return item
+    end
+  }
+  return nil
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  new_cart = [];
+  cart.each do
+    |item_in_cart|
+    
+    if not (find_item_by_name_in_collection(item_in_cart[:item], new_cart)) then
+      new_cart.push(
+        {
+          :item => item_in_cart[:item],
+          :price => item_in_cart[:price],
+          :clearance => item_in_cart[:clearance],
+          :count => 1
+        }
+        )
+    else
+      new_cart.each{|item| if item[:item] == item_in_cart[:item] then item[:count] += 1 end}
+    end
+  end
+  new_cart
 end
 
 
-  
+
+consolidate_cart([
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "AVOCADO", :price => 3.00, :clearance => true },
+  {:item => "KALE", :price => 3.00, :clearance => false},
+  {:item => "Apple", :price => 3.00, :clearance => false},
+  {:item => "KALE", :price => 3.00, :clearance => false},
+]);
